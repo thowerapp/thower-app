46242b74fed52829aec5f36faba9cd8d5015521d
Pierre Fabien <pierre.fabien.dev@gmail.com>
2026-04-05 15:01:09 +0200

    clean

--- routes/admin/sales/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { getAllTransactionsDashboard } from '$lib/prisma/transaction/getAllTransactionsDashboard';
-import type { PageServerLoad } from './$types';
-
-export const load = (async () => {
-	const transactions = await getAllTransactionsDashboard();
-	return {
-		transactions
-	};
-}) satisfies PageServerLoad;
--- routes/reglementations/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import type { PageProps } from './$types';
-
-    let { data }: PageProps = $props();
-</script>
-
-<h1>Reglementations</h1>
